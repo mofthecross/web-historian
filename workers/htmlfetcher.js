1cbae9d0ddf50a1a7c4a79889da4b30f81116101
mofthecross <mofthecross@gmail.com>
2016-06-30 14:59:34 -0700

    Create file that stores cron job and log of htmlfetcher.

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,19 @@
-// Use the code in `archive-helpers.js` to actually download the urls
-// that are waiting.
+var archive = require('../helpers/archive-helpers');
+
+archive.readListOfUrls(function(arrayOfUrls) {
+  var urlsToDownload = [];
+  var urlsChecked = 0;
+  arrayOfUrls.forEach(function(url) {
+    archive.isUrlArchived(url, function(isArchived) {
+      if (!isArchived) {
+        urlsToDownload.push(url);
+        console.log('Will download', url);
+      }
+
+      urlsChecked++;
+      if (arrayOfUrls.length === urlsChecked) {
+        archive.downloadUrls(urlsToDownload);
+      }
+    });
+  });
+});
